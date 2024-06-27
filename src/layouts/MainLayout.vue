0f7d5ea7e4600c0f6e63e85884cf8eb4a268c204
reigikai <155409721+reigikai@users.noreply.github.com>
2024-06-27 10:53:58 -0700

    Add files via upload
    moved files from personal github into reigikai account

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal bordered class="bg-primary text-black" height-hint="98">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+      </q-toolbar>
+
+    </q-header>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Explore below
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+  import { defineComponent, ref } from 'vue'
+  import EssentialLink from 'components/EssentialLink.vue'
+
+  const linksList = [
+    {
+      title: 'Instagram',
+      caption: '@reigikai',
+      icon: 'favorite',
+      link: 'https://www.instagram.com/reigikai/'
+    },
+    {
+      title: 'Facebook',
+      caption: '@skc.skif.lv',
+      icon: 'camera',
+      link: 'https://www.facebook.com/groups/skc.skif.lv'
+    },
+    {
+      title: 'TikTok',
+      caption: '@reigikai',
+      icon: 'slideshow',
+      link: 'https://www.tiktok.com/@reigikai'
+    },
+    {
+      title: 'Google Maps',
+      caption: 'Our location on Google Maps',
+      icon: 'public',
+      link: 'https://goo.gl/maps/Jurn65vKomTaMhjcA'
+    },
+    {
+      title: 'Apple Maps',
+      caption: 'If you prefer to use Apple Maps',
+      icon: 'apple',
+      link: 'https://maps.apple.com/?address=614%20E%20Sahara%20Ave,%20Unit%2013,%20Las%20Vegas,%20NV%20%2089104,%20United%20States&ll=36.144998,-115.145824&q=614%20E%20Sahara%20Ave,%20Unit%2013&_ext=EiYpEBX0GfwRQkAxeyJTULDJXMA5juoZdiITQkBBF9m5DfrIXMBQBA%3D%3D'
+    }
+  ]
+
+  export default defineComponent({
+    name: 'MainLayout',
+
+    components: {
+      EssentialLink
+    },
+
+    setup () {
+      const leftDrawerOpen = ref(false)
+
+      return {
+        essentialLinks: linksList,
+        leftDrawerOpen,
+        toggleLeftDrawer () {
+          leftDrawerOpen.value = !leftDrawerOpen.value
+        }
+      }
+    }
+  })
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,150 @@
+<template>
+  <q-page padding class="text-center">
+    <img
+      alt="Reigikai logo"
+      src="~assets/reigikai-logo.png"
+      style="width: 250px; height: 75px; vertical-align: middle"
+    />
+
+    <div class="my-font">
+      <div class="q-px-sm q-pt-sm q-mb-sm">
+        <div class="text-subtitle">
+          Shotokan Karate-do Club - SKIF Las Vegas
+        </div>
+      </div>
+    </div>
+
+    <h5 class="my-font">
+      Develop Character Through the Study of Shotokan Karate-do<br />
+    </h5>
+    <h5 class="my-font">
+      Reigikai Shotokan Karate-do Club is a Family-Oriented & Not-for-Profit
+      Karate Group<br />
+    </h5>
+    <h5 class="my-font">Participants will learn:</h5>
+    <h6 class="my-font">
+      Self-Discipline <br />Good Moral Character <br />Self-Defense
+      <br />Integrity <br />Perseverance <br />Authentic Traditional Japanese
+      Karate
+    </h6>
+
+    <div class="my-font">
+      <div class="q-pa-md">
+        <q-markup-table>
+          <thead>
+            <tr>
+              <th class="text-left">Class Schedule</th>
+              <th class="text-right">Time</th>
+              <th class="text-right">Rank/Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-left">Monday</td>
+              <td class="text-right">5:30pm - 6:30pm<br /></td>
+              <td class="text-right">Beginners<br />(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left"></td>
+              <td class="text-right">6:30pm - 8:00pm<br /></td>
+              <td class="text-right">
+                Intermediate, Advanced, and Black Belt<br />(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left">Wednesday</td>
+              <td class="text-right">5:30pm - 6:30pm<br /></td>
+              <td class="text-right">Beginners<br/>(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left"></td>
+              <td class="text-right">6:30pm - 8:00pm<br /></td>
+              <td class="text-right">
+                Intermediate, Advanced, and Black Belt<br />(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left">Friday</td>
+              <td class="text-right">7:30pm - 8:30pm</td>
+              <td class="text-right">Kumite Training<br>(Kids, Teens, Adults)</td>
+            </tr>
+
+            <tr>
+              <td class="text-left">Saturday</td>
+              <td class="text-right">9:30am - 10:30am</td>
+              <td class="text-right">
+                Advanced Kata Training<br />(Intermediate, Advanced, and Black Belt)</td>
+            </tr>
+            <tr>
+              <td class="text-left"></td>
+              <td class="text-right">10:30am - 11:45am<br></td>
+              <td class="text-right">Beginners<br>(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left"></td>
+              <td class="text-right">10:30am - 12:30pm<br></td>
+              <td class="text-right">Intermediate, Advanced, and Black Belt<br>(Kids, Teens, Adults)</td>
+            </tr>
+            <tr>
+              <td class="text-left">Sunday</td>
+              <td class="text-right">10:30am - 12:30pm</td>
+              <td class="text-right">Competition Team Training<br>(Drills, Core Training, Agility/Strength Conditioning)</td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+      </div>
+    </div>
+
+    <h4 class="my-font">Contact Us</h4>
+
+    <div class="my-font">
+      <div class="q-pa-md">
+        <q-markup-table>
+          <thead>
+            <tr>
+              <th class="text-left">Instructor</th>
+              <th class="text-right">Phone Number</th>
+              <th class="text-right">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-left">Sensei Ronald Tolentino</td>
+              <td class="text-right">702-493-5253<br /></td>
+              <td class="text-right">702karatedo@gmail.com</td>
+            </tr>
+            <tr>
+              <td class="text-left">Sensei Joseph Tolentino</td>
+              <td class="text-right">702-588-3460<br /></td>
+              <td class="text-right">joetolentino84@yahoo.com</td>
+            </tr>
+            <tr>
+              <td class="text-left">Sensei Joshua Tolentino</td>
+              <td class="text-right">702-324-2752<br /></td>
+              <td class="text-right"></td>
+            </tr>
+            <tr>
+              <td class="text-left">Sensei Steven Olivares</td>
+              <td class="text-right">702-712-6457<br /></td>
+              <td class="text-right"></td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+      </div>
+    </div>
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" color="accent" />
+    </q-page-scroller>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
